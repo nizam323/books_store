f349cad9039887312f556b7b5b23307480cd6fb7
nizam323 <muhammadnizamuddin.sce@gmail.com>
2024-12-14 13:23:32 +0500

    store signup user in database

--- src/UIs/Signin/index.jsx
+++ src/UIs/Signin/index.jsx
@@ -1,34 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../public/images/images.png"
-import styles from "./login.module.css"
+import styles from "../Signup/login.module.css"
 
-export default function Signin() {
+export default function SignIn() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit() {
+        fetch("http:/localhost:3000/signin"), {
+            method: "post",
+            Headers: {},
+            body: JSON.stringify(
+                {
+                    email,
+                    password
+                }
+            )
+        }
+    }
+
     return (
         <>
             <div className={styles.formcon}>
                 <div className={styles.logincon}>
-                        <div className={styles.img} style={{ backgroundImage: `url(${logo})`, borderRadius: "40px" }}></div>
+                    <div className={styles.img} style={{ backgroundImage: `url(${logo})`, borderRadius: "40px" }}></div>
                     <div className={styles.unameemail}>
-                        <form action="">
-                            <label htmlFor="u-name" style={{ color: '#3c434a' }}>Email Address</label>
-                            <input type="text" id="u-name" required />
+                        <form method="post" onSubmit={handleSubmit}>
+                            <label htmlFor="u-email" style={{ color: '#3c434a' }}>Email Address</label>
+                            <input className={styles.input} type="text" id="u-email" required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                             <br />
                             <br />
                             <label htmlFor="pswd" style={{ color: '#3c434a' }}>Password</label>
-                            <input type="password" id="pswd" required />
+                            <input className={styles.input} type="password" id="pswd" required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </form>
                     </div>
-                    <div className={styles.checkbtn}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                            <input type="checkbox" name="" id="" style={{ width: '17px', height: '17px', marginRight: '7px' }} />
-                            <p style={{ color: '#3c434a' }}>Remember Me</p>
+                    <center>
+                        <div className={styles.checkbtn} style={{ justifyContent: "center" }}>
+                            <button type="submit">Sign In</button>
                         </div>
-                        <button type="submit">Sign In</button>
-                    </div>
+                    </center>
                 </div>
                 <div className={styles.acon}>
                     <a>Lost your password?</a>
-                    <a>← Go to Brandstore</a>
+                    <a href="index.html">← Go to Brandstore</a>
                 </div>
             </div>
         </>
--- src/UIs/Signup/index.jsx
+++ src/UIs/Signup/index.jsx
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../public/images/images.png"
-import styles from "../Signin/login.module.css"
+import styles from "./login.module.css"
 
-export default function Signup() {
+export default function SignUp() {
+    const [username, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPass, setShowPass] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:3000/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            })
+        })
+        setName("");
+        setEmail("");
+        setPassword("");
+    }
+
     return (
         <>
             <div className={styles.formcon}>
                 <div className={styles.logincon}>
-                        <div className={styles.img} style={{ backgroundImage: `url(${logo})`, borderRadius: "40px" }}></div>
+                    <div className={styles.img} style={{ backgroundImage: `url(${logo})`, borderRadius: "40px" }}></div>
                     <div className={styles.unameemail}>
-                        <form action="">
-                            <label htmlFor="u-name" style={{ color: '#3c434a' }}>Email Address</label>
-                            <input type="text" id="u-name" required />
+                        <form method="POST" onSubmit={handleSubmit}>
+                            <label htmlFor="u-name" style={{ color: '#3c434a' }}>User Name</label>
+                            <input className={styles.input} type="text" id="u-name" required
+                                value={username}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <br />
+                            <br />
+                            <label htmlFor="u-email" style={{ color: '#3c434a' }}>Email Address</label>
+                            <input className={styles.input} type="email" id="u-email" required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                             <br />
                             <br />
                             <label htmlFor="pswd" style={{ color: '#3c434a' }}>Password</label>
-                            <input type="password" id="pswd" required />
+                            <input className={styles.input} type={showPass ? "text" : "password"} id="pswd" required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <br />
+                            <br />
+                            <center>
+                                <span style={{ fontSize: "16px", display: "flex", alignItems: "center", justifyContent: "center" }}>{showPass ? "Hide Password" : "Show Password"}
+                                    <pre> </pre>
+                                    <input onClick={() => setShowPass(!showPass)} style={{ cursor: "pointer" }} type="checkbox" /></span>
+                            </center>
+                            <center>
+                                <div className={styles.checkbtn}>
+                                    <button type="submit">Sign Up</button>
+                                </div>
+                            </center>
                         </form>
-                    </div>
-                    <div className={styles.checkbtn} style={{justifyContent:"center"}}>
-                        <button type="submit">Sign Up</button>
                     </div>
                 </div>
                 <div className={styles.acon}>
-                    <a href="index.html">← Go to Brandstore</a>
+                    <a>← Go to Brandstore</a>
                 </div>
             </div>
         </>
